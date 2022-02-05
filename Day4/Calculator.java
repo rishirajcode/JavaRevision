339eee0385e434b5af120246784fbfa8f3bd104a
Rishi Raj <1999rishiraj@gmail.com>
2022-02-05 16:08:10 +0530

    Update Calculator.java

--- Day4/Calculator.java
+++ Day4/Calculator.java
@@ -11,7 +11,6 @@ class Calculator {
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
 
-    // ask users to enter numbers
     System.out.println("Enter first number");
     number1 = input.nextDouble();
 
